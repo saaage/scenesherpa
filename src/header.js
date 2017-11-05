7ea285660e350f688281052924770bb5f3a80970
Sage911 <stefan.age@outlook.com>
2017-11-05 15:36:28 -0600

    Fixed import/no-unresolved issue
    ESLint config should utilize “webpack” as the value for
    settings.import/resolver

--- src/header.js
+++ src/header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 const Header = () =>
-  <h1>Welcome to Scene Sherpa</h1>
+  <h1>Welcome to Scene Sherpa :)</h1>
 
 export default Header
--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
 const path = require('path')
 const webpack = require('webpack')
 
-module.exports = (env) => {
-  console.log('NODE_ENV:', env.NODE_ENV)
-
-  return {
-    context: path.resolve(__dirname, 'src'),
-    devServer: {
-      contentBase: path.join(__dirname, 'dist'),
-      compress: true,
-      historyApiFallback: true,
-      hot: true,
-      port: 9000,
-      stats: 'minimal'
-    },
-    entry: './index.js',
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
-      ]
-    },
-    performance: {
-      hints: process.env.NODE_ENV === 'production' ? 'warning' : false
-    },
-    plugins: [
-      new webpack.NamedModulesPlugin(),
-      new webpack.HotModuleReplacementPlugin()
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+    port: 9000,
+    stats: 'minimal'
+  },
+  entry: './index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  performance: {
+    hints: process.env.NODE_ENV === 'production' ? 'warning' : false
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
+  resolve: {
+    modules: [
+      path.join(__dirname, 'src'),
+      'node_modules'
     ]
   }
 }
