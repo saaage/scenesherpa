11988105fcb4190e2a789a06479120dc2f42c117
Sage911 <stefan.age@outlook.com>
2017-11-06 12:18:19 -0600

    Installed babel-plugin-transform-class-properties
    Allows us to bind methods to instance of the component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
-
-import Header from 'Header'
-import Actor from 'components/Actor'
-import Movie from 'components/Movie'
 import Welcome from 'components/Welcome'
-import SimpleNav from 'components/SimpleNav'
 
-const App = () => (
-  <div>
-    <SimpleNav />
-    <Header />
-    <Route exact path="/" component={Welcome} />
-    <Route path="/actor" component={Actor} />
-    <Route path="/movie" component={Movie} />
-  </div>
-)
+const App = () =>
+  <Welcome />
 
 export default App
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Welcome = () =>
-  <h2>Welcome!</h2>
+class Welcome extends Component {
+  state = {
+    query: ''
+  }
+
+  handleChange = () => {
+    this.setState({
+      query: this.search.value
+    })
+  }
+
+  logState = () => {
+    console.log(this.state.query)
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Scene Sherpa</h1>
+        <input
+          type="text"
+          autoComplete="on"
+          ref={input => this.search = input}
+          onChange={this.handleChange}
+        />
+        <button onClick={this.logState}>Search</button>
+      </div>
+    )
+  }
+}
 
 export default Welcome
