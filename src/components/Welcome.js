229a51204f4c3de739f555498a1aa8e5d6675035
Sage911 <stefan.age@outlook.com>
2017-11-06 15:36:49 -0600

    Installed axios for XMLHttprequests
    Automatically parses incoming data for us. Also allows us to use .then
    .catch for better error handling vs fetch API

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -15,16 +15,34 @@ class Welcome extends Component {
     console.log(this.state.query)
   }
 
+  hinter = () => {
+    let input = this.state.query
+    let { hugeList } = this
+    const min_characters = 0
+
+    if (input.value.length < min_characters) {
+      return null
+    } else {
+      // code here to exit any pending API calls
+      // if async fetch function is resolved
+        // clear any items currently in datalist
+        // for each item in response, create an option element
+          // attach the option to the datalist element
+    }
+  }
+ 
   render() {
     return (
       <div>
         <h1>Welcome to Scene Sherpa</h1>
-        <input
-          type="text"
-          autoComplete="on"
-          ref={input => this.search = input}
-          onChange={this.handleChange}
-        />
+        <form>
+          <input
+            type="text"
+            ref={input => this.search = input}
+            onChange={this.handleChange}
+          />
+          <datalist ref={list => this.hugeList = list} />
+        </form>
         <button onClick={this.logState}>Search</button>
       </div>
     )
