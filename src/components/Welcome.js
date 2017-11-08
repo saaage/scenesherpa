8578d39641a2c37ac4244322ff039476930e0270
Sage911 <stefan.age@outlook.com>
2017-11-07 20:31:15 -0600

    Controlled input provides suggestions
    onChange hits API for possible search items based on the value of the
    input field and renders items to the screen.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,49 +1,62 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+const { API_KEY } = process.env
+const API_URL = 'http://api.themoviedb.org/3'
 
 class Welcome extends Component {
   state = {
-    query: ''
+    query: '',
+    results: []
   }
 
-  handleChange = () => {
+  getResults = () => {
+    axios.get(`${API_URL}/search/multi?api_key=${API_KEY}&query=${this.state.query}&page=1`)
+      .then(({ data }) => this.setState(({ results: data.results })))
+      .catch(e => console.log(e))
+  }
+
+  clearResults = () => {
+    console.log('WIP')
+  }
+
+  handleChange = (e) => {
+    e.preventDefault()
     this.setState({
       query: this.search.value
+    }, () => {
+      if (this.state.query) {
+        this.getResults()
+      } else if (!this.state.query) {
+        this.clearResults()
+      }
     })
   }
 
-  logState = () => {
-    console.log(this.state.query)
-  }
+  handleSubmit = e => e.preventDefault()
 
-  hinter = () => {
-    let input = this.state.query
-    let { hugeList } = this
-    const min_characters = 0
-
-    if (input.value.length < min_characters) {
-      return null
-    } else {
-      // code here to exit any pending API calls
-      // if async fetch function is resolved
-        // clear any items currently in datalist
-        // for each item in response, create an option element
-          // attach the option to the datalist element
-    }
-  }
- 
   render() {
+    const { results } = this.state
+
+    const output = results.map(obj => (
+      <p key={obj.id}>{obj.title}</p>
+    ))
+
     return (
       <div>
         <h1>Welcome to Scene Sherpa</h1>
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <input
             type="text"
             ref={input => this.search = input}
             onChange={this.handleChange}
+            placeholder="Search for..."
           />
-          <datalist ref={list => this.hugeList = list} />
+          <button>Go</button>
         </form>
-        <button onClick={this.logState}>Search</button>
+        <ul>
+          {output}
+        </ul>
       </div>
     )
   }
