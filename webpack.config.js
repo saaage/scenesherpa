7ef2b16bc3206f4ce10f9717fd52f78334000182
Sage911 <stefan.age@outlook.com>
2017-11-07 20:27:17 -0600

    Modified webpack config and dev script
    Allows us to pass environment variables so we can keep our api
    key/secrets out of client code.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,49 @@
 const path = require('path')
 const webpack = require('webpack')
 
-module.exports = {
-  context: path.resolve(__dirname, 'src'),
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    historyApiFallback: true,
-    hot: true,
-    port: 9000,
-    stats: 'minimal'
-  },
-  entry: './index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-    ]
-  },
-  performance: {
-    hints: process.env.NODE_ENV === 'production' ? 'warning' : false
-  },
-  plugins: [
-    new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  resolve: {
-    modules: [
-      path.join(__dirname, 'src'),
-      'node_modules'
-    ]
+module.exports = (env) => {
+
+  if (env.API_KEY) {
+    console.log('\x1b[36m%s\x1b[0m', '\nAPI key loaded\n')
+  } else {
+    console.log('\x1b[33m%s\x1b[0m: ', '\nWarning - No API_KEY declared\n')
+  }
+
+  return {
+    context: path.resolve(__dirname, 'src'),
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      historyApiFallback: true,
+      hot: true,
+      port: 9000,
+      stats: 'minimal'
+    },
+    entry: './index.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+      ]
+    },
+    performance: {
+      hints: process.env.NODE_ENV === 'production' ? 'warning' : false
+    },
+    plugins: [
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      })
+    ],
+    resolve: {
+      modules: [
+        path.join(__dirname, 'src'),
+        'node_modules'
+      ]
+    }
   }
 }
