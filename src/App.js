8834bb17b735b94290819b86f2dd468e95c326fe
Sage911 <stefan.age@outlook.com>
2017-11-09 16:21:06 -0600

    App gets new API configuration every 2 days
    Information is cached in localstorage. Allows us to dynamically hit the
    API using a URL that could change daily.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
+import axios from 'axios'
 
 import MovieContainer from 'containers/MovieContainer'
 import TVContainer from 'containers/TVContainer'
 import Search from 'containers/Search'
 import ActorContainer from 'containers/ActorContainer'
 
-const App = () => (
-  <Switch>
-    <Route exact path="/" component={Search} />
-    <Route path="/actor/:id" component={ActorContainer} />
-    <Route path="/movie/:id" component={MovieContainer} />
-    <Route path="/tv/:id" component={TVContainer} />
-  </Switch>
-)
+const { API_KEY } = process.env
+const API_URL = 'https://api.themoviedb.org/3'
+
+class App extends Component {
+  componentDidMount() {
+    const now = new Date()
+    const configDate = localStorage.getItem('configDate')
+    const daysElapsed = Math.ceil(((now - Date.parse(configDate)) / 8.64e7))
+    if (configDate && daysElapsed >= 2) {
+      this.getAPIConfig()
+    } else if (!configDate) {
+      this.getAPIConfig()
+    }
+  }
+
+  getAPIConfig = () => {
+    axios.get(`${API_URL}/configuration?api_key=${API_KEY}`)
+      .then(({ data }) => {
+        localStorage.setItem('images', JSON.stringify(data.images))
+        localStorage.setItem('configDate', new Date())
+      })
+      .catch(e => console.log(e))
+  }
+
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route path="/actor/:id" component={ActorContainer} />
+        <Route path="/movie/:id" component={MovieContainer} />
+        <Route path="/tv/:id" component={TVContainer} />
+      </Switch>
+    )
+  }
+}
 
 export default App
