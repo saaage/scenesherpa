c8e4f5160345234d7bfc7c9e95792a4ae4f4e2b6
Sage911 <stefan.age@outlook.com>
2017-11-12 16:18:10 -0600

    MovieCredits and TVCredits display results inside of styled-components
    ScreenSizedDiv keeps page from jumping up during loading and MediaList
    is just a modified <ul>

--- src/containers/TVCredits.js
+++ src/containers/TVCredits.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import styled from 'styled-components'
 
-import MovieListItem from 'components/MovieListItem'
+import TVListItem from 'components/TVListItem'
 import ScreenSizedDiv from 'styled/ScreenSizedDiv'
 import MediaList from 'styled/MediaList'
 
 const { API_KEY } = process.env
 const API_URL = 'https://api.themoviedb.org/3'
 
-class MovieCredits extends Component {
+class TVCredits extends Component {
   state = {
     isLoading: true,
     credits: {}
@@ -20,7 +19,7 @@ class MovieCredits extends Component {
   }
 
   getCredits = () => {
-    axios.get(`${API_URL}/person/${this.props.id}/movie_credits?api_key=${API_KEY}&language=en-US`)
+    axios.get(`${API_URL}/person/${this.props.id}/tv_credits?api_key=${API_KEY}&language=en-US`)
       .then(({ data }) => {
         this.setState({
           credits: data,
@@ -33,15 +32,15 @@ class MovieCredits extends Component {
     if (this.state.isLoading) {
       return (
         <ScreenSizedDiv>
-          <h3>Loading movie credits...</h3>
+          <h3>Loading TV credits...</h3>
         </ScreenSizedDiv>
       )
     }
-    const credits = this.state.credits.cast.map((m) => {
-      return <MovieListItem key={m.id} {...m} />
+    const credits = this.state.credits.cast.map((t) => {
+      return <TVListItem key={t.id} {...t} />
     })
     return <MediaList>{credits}</MediaList>
   }
 }
 
-export default MovieCredits
+export default TVCredits
