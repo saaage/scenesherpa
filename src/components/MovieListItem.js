544a4a2100bd20fce2969b5374993ceb23656a6a
Sage911 <stefan.age@outlook.com>
2017-11-12 20:43:38 -0600

    Added styles to Movie and TV items

--- src/components/MovieListItem.js
+++ src/components/MovieListItem.js
@@ -1,20 +1,51 @@
 import React from 'react'
 import getYear from 'date-fns/get_year'
+import styled from 'styled-components'
 
 const MovieListItem = (props) => {
   const images = JSON.parse(localStorage.getItem('images'))
   if (props.poster_path) {
     return (
-      <li>
-        <img src={`${images.base_url}${images.poster_sizes[1]}${props.poster_path}`} alt={props.title} />
-        <br />
-        {props.title} ({getYear(props.release_date)})
-        <br />
+      <StyledListItem>
+        <div>
+          <img
+            src={`${images.base_url}${images.poster_sizes[1]}${props.poster_path}`}
+            alt={props.title}
+          />
+        </div>
+        <p>{props.title} ({getYear(props.release_date)})</p>
         <b><em>{props.character ? props.character : null}</em></b>
-      </li>
+      </StyledListItem>
     )
   }
   return null
 }
 
+const StyledListItem = styled.li`
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: #4F4F4F;
+  font-family: Alegreya;
+  margin-bottom: 8px;
+  margin-left: 8px;
+  margin-right: 8px;
+  height: 372px;
+  div {
+    height: 258px;
+    overflow: hidden;
+  }
+  p , b {
+    display: block;
+    max-width: 154px;
+    min-height: 44px;
+    max-height: 44px;
+    overflow: hidden;
+    padding: 0px;
+    margin: 0px;
+  }
+  b {
+    margin-top: auto;
+    margin-bottom: auto;
+  }
+  overflow: hidden;
+`
 export default MovieListItem
--- src/styled/MediaList.js
+++ src/styled/MediaList.js
@@ -2,6 +2,9 @@ import styled from 'styled-components'
 
 const MediaList = styled.ul`
   list-style-type: none;
+  padding-left: 0px;
+  li {
+    display: inline-block;
 `
 
 export default MediaList
