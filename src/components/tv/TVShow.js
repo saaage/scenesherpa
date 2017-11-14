64dc42cdbf1d6d17f1c9e33951829b440d55d099
Sage911 <stefan.age@outlook.com>
2017-11-14 15:53:54 -0600

    TVShow renders in the same manner as Movie/Actor

--- src/components/tv/TVShow.js
+++ src/components/tv/TVShow.js
@@ -1,5 +1,32 @@
 import React from 'react'
+import CastCrew from 'containers/people/CastCrew'
+import Backdrop from 'styled/Backdrop'
+import SubjectImage from 'styled/SubjectImage'
 
-const TVShow = () => <h1>Welcome to TV Show Page</h1>
+const TVShow = (props) => {
+  const images = JSON.parse(localStorage.getItem('images'))
+  const {
+    id, credits, details, similar, videos
+  } = props
+  return (
+    <div>
+      <Backdrop>
+        <img
+          src={`${images.base_url}${images.backdrop_sizes[3]}${details.backdrop_path}`}
+          alt={details.name}
+        />
+      </Backdrop>
+      <SubjectImage>
+        <img
+          src={`${images.base_url}${images.poster_sizes[5]}${details.poster_path}`}
+          alt={details.name}
+        />
+      </SubjectImage>
+      <h2>{details.name}</h2>
+      <p>{details.overview}</p>
+      <CastCrew credits={credits} />
+    </div>
+  )
+}
 
 export default TVShow
