79d5698a649d721f136bbd17eaa27f6b0cce8580
Sage911 <stefan.age@outlook.com>
2017-11-17 10:22:48 -0600

    Adjusting Nav styles
    Nav sticks when it reaches the top of the viewport. Allows us to put
    items, messages,  etc. above Nav if needed.

--- src/styled/Input.js
+++ src/styled/Input.js
@@ -2,6 +2,8 @@ import styled from 'styled-components'
 
 const Input = styled.input`
   font-size: 16px;
+  margin-left: 8px;
+  height: 2em;
   width: 80%;
 `
 
---
+++ src/styled/Nav.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const Nav = styled.nav`
+  background: white;
+  min-height: 2em;
+  position: sticky;
+  top: 0;
+  width: 100vw;
+  margin-bottom: 8px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  form {
+    padding-top: 1em;
+    padding-bottom: 1em;
+  }
+`
+
+export default Nav
