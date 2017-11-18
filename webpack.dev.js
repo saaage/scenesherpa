6a35d7dd2cbad90fd570b2f13bc58e5c76fc5512
Sage911 <stefan.age@outlook.com>
2017-11-18 14:31:31 -0600

    Setting up for Heroku

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,16 +1,16 @@
 const path = require('path')
+const merge = require('webpack-merge')
 const webpack = require('webpack')
+const common = require('./webpack.common.js')
 
 module.exports = (env) => {
-
   if (env.API_KEY) {
     console.log('\x1b[36m%s\x1b[0m', '\nAPI key loaded\n')
   } else {
     console.log('\x1b[33m%s\x1b[0m: ', '\nWarning - No API_KEY declared\n')
   }
 
-  return {
-    context: path.resolve(__dirname, 'src'),
+  return merge(common, {
     devServer: {
       contentBase: path.join(__dirname, 'dist'),
       compress: true,
@@ -22,36 +22,12 @@ module.exports = (env) => {
       port: 9000,
       stats: 'minimal'
     },
-    entry: ['babel-polyfill', 'index.js'],
-    output: {
-      filename: 'bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/'
-    },
-    module: {
-      rules: [
-        { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
-        {
-          test: /\.jpe?g$|\.ico$|\.gif$|\.png$|\.svg$|\.woff$|\.ttf$|\.wav$|\.mp3$/,
-          loader: 'file-loader?name=[name].[ext]'
-        }
-      ]
-    },
-    performance: {
-      hints: process.env.NODE_ENV === 'production' ? 'warning' : false
-    },
     plugins: [
       new webpack.NamedModulesPlugin(),
       new webpack.HotModuleReplacementPlugin(),
       new webpack.DefinePlugin({
         'process.env.API_KEY': JSON.stringify(env.API_KEY)
       })
-    ],
-    resolve: {
-      modules: [
-        path.join(__dirname, 'src'),
-        'node_modules'
-      ]
-    }
-  }
+    ]
+  })
 }
