41454d675c15ca54c74fcbac74ff25e567262d57
Sage911 <stefan.age@outlook.com>
2017-11-18 17:26:34 -0600

    API_KEY is pulled at build time

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,20 @@
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const webpack = require('webpack')
 
-module.exports = merge(common, {
-  plugins: [
-    new UglifyJSPlugin()
-  ]
-})
+module.exports = (env) => {
+  if (env.API_KEY) {
+    console.log('\x1b[36m%s\x1b[0m', '\nAPI key loaded\n')
+  } else {
+    console.log('\x1b[33m%s\x1b[0m: ', '\nWarning - No API_KEY declared\n')
+  }
+  return merge(common, {
+    plugins: [
+      new UglifyJSPlugin(),
+      new webpack.DefinePlugin({
+        'process.env.API_KEY': JSON.stringify(env.API_KEY)
+      })
+    ]
+  })
+}
