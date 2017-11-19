e68daf7c83f99e4775e54d55d2b6cc38f1b97181
Sage911 <stefan.age@outlook.com>
2017-11-18 18:22:37 -0600

    Changing folder structure for Heroku
    >:|

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 
 const app = express()
-const port = process.env.PORT
+const port = process.env.PORT || 8080
 
 app.use(express.static(path.join(__dirname, 'dist')))
 app.get('/', (req, res) => {
