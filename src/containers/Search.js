7a865fa8d4611699ab21101408ecde676f2d2c8e
Sage911 <stefan.age@outlook.com>
2017-11-23 14:41:24 -0600

    Drop down only shows if Search has made an API call

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -51,7 +51,7 @@ class Search extends Component {
     this.setState({
       query: this.search.value
     }, () => {
-      if (this.state.query) {
+      if (this.state.query && this.state.query.length > 1) {
         this.showDropdown()
         if (this.state.query.length % 2 === 0) {
           this.getResults()
