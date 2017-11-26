1d2855a79c7782c7c31d396ccbfe4bfe1fa12349
Sage911 <stefan.age@outlook.com>
2017-11-26 17:22:29 -0600

    Moved index from dist to app

--- app/server.js
+++ app/server.js
@@ -4,8 +4,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
 
-app.use(express.static(path.join(__dirname, 'dist')))
+app.use(express.static(path.join(__dirname, '/')))
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist/index.html'))
+  res.sendFile(path.resolve(__dirname, 'index.html'))
 })
 app.listen(port)
