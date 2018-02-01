1212ce9b0c396245c6d9bd0f7092d9e2d1b323e3
Sage911 <stefan.age@outlook.com>
2018-01-31 23:49:56 -0600

    Building Welcome page
    Added a tagline, footer, relocated SherpaLogo

--- src/containers/welcome/Welcome.js
+++ src/containers/welcome/Welcome.js
@@ -2,54 +2,66 @@ import React from 'react'
 import styled from 'styled-components'
 
 import Search from 'containers/Search'
+import Footer from 'components/footer/Footer'
 
 import SherpaLogo from 'components/SherpaLogo'
 
 const Welcome = () => (
-  <Layout>
-    <h2 className="title">Scene Sherpa</h2>
+  <WelcomeLayout>
     <SherpaLogo
       className="logo"
       color="#5B97BA"
-      width="117"
-      height="225"
+      width="57"
+      height="110"
     />
-    <Search className="search" />
-  </Layout>
+    <h1 className="title">Scene Sherpa</h1>
+    <p className="tagline">Tell us what you're looking for...</p>
+    <Search />
+    <div className="footer__div">
+      <Footer className="footer" />
+    </div>
+  </WelcomeLayout>
 )
 
 // Layout styles are only concerned with positioning of the elements
 //  inside. Layout should not be modifying the styles of the components
 //  that live inside.
-const Layout = styled.div`
+const WelcomeLayout = styled.div`
   display: flex;
   flex-direction: column;
+  height: 100%;
+  margin-bottom: 1.5rem;
+
+  .footer__div {
+    display: flex;
+    flex: 1;
+    flex-direction: column;
+    justify-content: flex-end;
+    margin-top: 2rem;
+  }
 
   .title {
     color: #5B97BA;
-    margin: 1em 1em 2em .5em;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 0;
     font-family: sans-serif;
     font-weight: 600;
+  }
+
+  .tagline {
+    color: #5B97BA;
+    font-family: 'Open Sans';
+    font-size: 1.4rem;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 0;
   }
 
   .logo {
     align-self: center;
     margin-bottom: 1em;
     margin-top: 3em;
-  }
-
-  .search {
-    align-items: center;
-    align-self: center;
-    border-radius: 5px;
-    border: 1px solid gray;
-    display: flex;
-    min-height: 40px;
-    width: 97vw;
-
-    form {
-      margin: 0 0 0 0;
-    }
   }
 `
 
