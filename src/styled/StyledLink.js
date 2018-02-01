b7af786f9e7f41d105cb0caed6062b4ca7a619ca
Sage911 <stefan.age@outlook.com>
2018-02-01 11:53:14 -0600

    Created a StyledLink component
    Removes text-decoration and changes color to dark gray

---
+++ src/styled/StyledLink.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const StyledLink = styled(Link)`
+  color: #696969;
+  font-family: 'Open Sans';
+  font-weight: bold;
+  text-decoration: none;
+`
+
+export default StyledLink
