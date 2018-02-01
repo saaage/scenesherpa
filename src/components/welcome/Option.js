3729f2105ab6fa750bc40753023412207b8896bc
Sage911 <stefan.age@outlook.com>
2018-02-01 11:53:43 -0600

    Option utilizes the StyledLink component
    To display search results in gray

--- src/components/welcome/Option.js
+++ src/components/welcome/Option.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import StyledLink from 'styled/StyledLink'
 import styled from 'styled-components'
 
 const Option = (props) => {
   if (props.media_type === 'person') {
     return (
-      <Link to={`/actor/${props.id}`}>
+      <StyledLink to={`/actor/${props.id}`}>
         <Button 
           onMouseDown={props.stopBlur}
           onMouseUp={props.mouseUp}
@@ -13,11 +13,12 @@ const Option = (props) => {
         >
           {props.name}
         </Button>
-      </Link>
+      </StyledLink>
     )
   } else if (props.media_type === 'tv') {
     return (
-      <Link to={`/tv/${props.id}`}>
+      <StyledLink
+        to={`/tv/${props.id}`}>
         <Button
           onMouseDown={props.stopBlur}
           onMouseUp={props.mouseUp}
@@ -25,11 +26,11 @@ const Option = (props) => {
         >
           {props.name}
         </Button>
-      </Link>
+      </StyledLink>
     )
   } else if (props.media_type === 'movie') {
     return (
-      <Link to={`/movie/${props.id}`}>
+      <StyledLink to={`/movie/${props.id}`}>
         <Button
           onMouseDown={props.stopBlur}
           onMouseUp={props.mouseUp}
@@ -37,13 +38,24 @@ const Option = (props) => {
         >
           <span className="optionTitle">{props.title}</span>
         </Button>
-      </Link>
+      </StyledLink>
     )
   }
   return null
 }
 
 const Button = styled.button`
+  display: block;
+  height:2.5em;
+  background: none;
+  border: 1px solid lightgray;
+  border-top: 0;
+  color: inherit;
+  font: inherit;
+  line-height: normal;
+  overflow: hidden;
+  padding: 4px 4px 4px .5rem;
+  text-align: left;
   width: 100%;
 
   .optionTitle {
